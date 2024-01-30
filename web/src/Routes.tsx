cd2eead7b74d96993c223f21a1ecb428673850fd
Rafael Medeiros <85957449+rafaelmedrib@users.noreply.github.com>
2024-01-30 00:26:04 +0000

    Add layout and tailwind configuration

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -7,12 +7,15 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Router, Route } from '@redwoodjs/router'
+import { Router, Route, Set } from '@redwoodjs/router'
+import MainLayout from './layouts/MainLayout/MainLayout'
 
 const Routes = () => {
   return (
     <Router>
-      <Route path="/" page={HomePage} name="home" />
+      <Set wrap={MainLayout}>
+        <Route path="/" page={HomePage} name="home" />
+      </Set>
       <Route notfound page={NotFoundPage} />
     </Router>
   )
